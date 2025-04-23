85f5472ed0702dffba2072abc2c1be77b03e8119
romeo <romeomeshack34@gmail.com>
2025-04-23 18:11:51 +0100

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+/* ---------------------------------------- */
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      input: {
+        ["index.html"]: resolve(__dirname, "index.html"),
+        ["index.css"]: resolve(__dirname, "index.css"),
+        popup: resolve(__dirname, "src/popup/main.tsx"),
+        content: resolve(__dirname, "src/content-scripts/main.ts"),
+        background: resolve(__dirname, "src/background-tasks/main.ts"),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          // Place content script directly under dist/
+          if (chunkInfo.name === "index.css") {
+            return "[name].css";
+          }
+
+          if (chunkInfo.name === "content") {
+            return "content-scripts/[name].js";
+          }
+
+          if (chunkInfo.name === "background") {
+            return "background-tasks/[name].js";
+          }
+
+          // Default entry for other output in the 'popup' dir
+          return "[name].js";
+        },
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+    outDir: "dist",
+  },
+  publicDir: "public",
+});
